44407d3e5720c44daef6dbcbcc4222045f105bd9
guoxiaoqiang <guoxiaoqiang@126.com>
2017-06-16 19:08:06 +0800

    test

--- backend/controllers/ApiController.php
+++ backend/controllers/ApiController.php
@@ -1,1 +1,28 @@
 <?php
+namespace backend\controllers;
+
+
+use backend\modules\alert\controllers\DefaultController;
+use Yii;
+use yii\web\Controller;
+
+
+/**
+ * 所有客户端请求的入口文件
+ * Class ApiController
+ * @package backend\controllers
+ */
+class ApiController extends Controller{
+
+    public function actionIndex(){
+//        return $this->runController();
+        //$request = file_get_contents("input://");
+        //$requestArr = json_encode($request,true);
+        return (new DefaultController('alert',Yii::$app->modules))->runAction('index');
+    }
+
+
+    public function call(){
+
+    }
+}
